9876dc620f70f2ed76fc4cf2791f5b78145c048b
AlexHorbatiuk <a2022@MBP-2022.fritz.box>
2024-07-07 13:56:19 +0200

    Init commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import reportWebVitals from './reportWebVitals';
 import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -18,5 +17,3 @@ root.render(
         </React.StrictMode>
     </I18nextProvider>
 );
-
-reportWebVitals();
